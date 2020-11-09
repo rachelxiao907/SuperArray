21751331766b430085af736cda7f19ea1918b28a
Rachel Xiao <rachelxiao907@gmail.com>
2020-11-08 20:35:42 -0500

    Added zip

--- Demo.java
+++ Demo.java
@@ -20,4 +20,22 @@ public class Demo {
     return arr;
   }
 
+  public static SuperArray zip(SuperArray a, SuperArray b) {
+    SuperArray arr = new SuperArray();
+    for (int i = 0; i < Math.min(a.size(), b.size()); i++) {
+      arr.add(a.get(i));
+      arr.add(b.get(i));
+    }
+    if (a.size() > b.size()) {
+      for (int j = b.size(); j < a.size(); j++) {
+        arr.add(a.get(j));
+      }
+    } else if (b.size() > a.size()) {
+      for (int j = a.size(); j < b.size(); j++) {
+        arr.add(b.get(j));
+      }
+    }
+    return arr;
+  }
+
 }
