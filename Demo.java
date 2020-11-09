e312b0497f6753569bf6ac73fe68be0017cac5d0
Rachel Xiao <rachelxiao907@gmail.com>
2020-11-08 19:52:44 -0500

    Added findOverlap

--- Demo.java
+++ Demo.java
@@ -8,15 +8,16 @@ public class Demo {
     }
   }
 
-  public static void main(String[]args) {
-    SuperArray words = new SuperArray();
-    //grouped to save vertical space
-    words.add("kani");   words.add("uni");     words.add("ebi");     words.add("una");
-    words.add("una");    words.add("ebi");     words.add("kani");    words.add("una");
-    words.add("una");    words.add("ebi");     words.add("toro");
-
-    System.out.println(words);
-    removeDuplicates(words);
-    System.out.println(words);
+  public static SuperArray findOverlap(SuperArray a, SuperArray b) {
+    removeDuplicates(a);
+    removeDuplicates(b);
+    SuperArray arr = new SuperArray();
+    for (int i = 0; i < a.size(); i++) {
+      if (b.contains(a.get(i))) {
+        arr.add(a.get(i));
+      }
+    }
+    return arr;
   }
+
 }
